a2cba48b4f765b8e905467eceb1b9c3f0110e345
sarsninja <infiniterepeater@gmail.com>
2022-01-29 13:25:29 -0600

    add script to continue multi download

---
+++ cont_mdl.sh
@@ -0,0 +1,8 @@
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_aa > log1.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_ab > log2.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_ac > log3.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_ad > log4.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_ae > log5.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_af > log6.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_ag > log7.txt &
+youtube-dl --continue --ignore-errors --no-overwrites -f "[filesize<200M]" --batch-file dlist_ah > log8.txt &
